4c10e2b6f1dc9814129c2fa4c1e76214f2472aff
Jordan Hilado <jordanalihilado@gmail.com>
2021-08-03 11:29:16 -0700

    Minor text changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Logo from './partials/Logo';
 
 const propTypes = {
@@ -112,16 +112,13 @@ const Header = ({
                       navPosition && `header-nav-${navPosition}`
                     )}>
                     <li>
-                      <Link to="#0" onClick={closeMenu}>Documentation</Link>
+                      <a href="https://central-1.gitbook.io/central/" target="_blank" rel="noopener noreferrer" onClick={closeMenu}>Documentation</a>
                     </li>
                   </ul>
                   {!hideSignin &&
                     <ul
                       className="list-reset header-nav-right"
                     >
-                      <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
-                      </li>
                     </ul>}
                 </div>
               </nav>
