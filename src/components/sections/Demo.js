a8ae445340d2ee8824e6c3ccdb64b89630514f3b
Jordan Hilado <jordanalihilado@gmail.com>
2021-08-07 17:46:17 -0700

    Connected backend search querying with frontend

--- src/components/sections/Demo.js
+++ src/components/sections/Demo.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Demo.css";
 import Logo from '../layout/partials/Logo'
 
+import axios from "axios"
+
 const Demo = () => {
+
+    const baseURL = "https://central-server-13.herokuapp.com"
+    let stackoverflow = []
+    let youtube = []
+    let google = []
+
+    const [searchText, setSearchText] = useState("how to center a div")
 
     return (
         <>
@@ -12,9 +21,12 @@ const Demo = () => {
                     <h1 className="headTitle mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">Live Demo</h1>
                     <p className="m-0 mb-32 reveal-from-bottom description" data-reveal-delay="300">Use the search bar below to search for content from Stack Overflow and YouTube.</p>
                     <div className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="600">
-                        <form action="">
-                            <input type="search"/>
-                            <i class="fa fa-search"></i>
+                        <form action="" onSubmit={(e) => {
+                            e.preventDefault()
+                            axios.get(`${baseURL}/?query=${searchText}`).then((res) => {stackoverflow.push(res.data.stackoverflow); youtube.push(res.data.youtube); google.push(res.data.google); console.log(stackoverflow, youtube, google)})
+                        }}>
+                            <input value={searchText} onChange={(e) => setSearchText(e.target.value)} type="search"/>
+                            <button><i class="fa fa-search"></i></button>
                         </form>
                     </div>
                 </div>
